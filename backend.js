1e0d54df8d29d9cd5055f4a6a421f1e4bbacdce2
Ashwath <ashwath.singh2005@gmail.com>
2025-06-07 12:58:19 +0530

    Completed Calculator with a working front-end and all challenges completed

--- backend.js
+++ backend.js
@@ -23,35 +23,93 @@ function multiplyNumbers(num1, num2) {
 }
 
 // BACKEND
-
 // input from the user as a string
-let userPrompt = prompt(
-  "Please enter the mathematical expression you would like to evaluate"
-);
 
-userPrompt = userPrompt.split(" ");
+/*let userPrompt = prompt(
+  "Please enter the mathematical expression you would like to evaluate"
+);*/
 
 // process the input to find which function is required and also if it contains multiple operators
-let calculation = 0;
-for (let i = 0; i < userPrompt.length; i++) {
-  // if doesn't below condition must be a number
 
-  if (userPrompt.at(i) === "+") {
-    console.log("add");
-    calculation = addNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
-    userPrompt[i+1] = calculation;
-  } else if (userPrompt.at(i) === "-") {
-    console.log("subtract");
-    calculation = subtractNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
-  } else if (userPrompt.at(i) === "*") {
-    console.log("multiple");
-    calculation = multiplyNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
-    userPrompt.at(i+1) = calculation;
-    userPrompt[i+1] = calculation;
-  } else if (userPrompt.at(i) === "/") {
-    console.log("divide");
-    calculation = divideNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
-    userPrompt[i+1] = calculation;
+function calculateExpression(userPrompt) {
+  userPrompt = userPrompt.split(" ");
+  let calculation = 0;
+  for (let i = 0; i < userPrompt.length; i++) {
+    // if doesn't below condition must be a number
+
+    if (userPrompt.at(i) === "+") {
+      calculation = addNumbers(
+        parseInt(userPrompt.at(i - 1)),
+        parseInt(userPrompt.at(i + 1))
+      );
+      userPrompt[i + 1] = calculation;
+    } else if (userPrompt.at(i) === "-") {
+      calculation = subtractNumbers(
+        parseInt(userPrompt.at(i - 1)),
+        parseInt(userPrompt.at(i + 1))
+      );
+    } else if (userPrompt.at(i) === "*") {
+      calculation = multiplyNumbers(
+        parseInt(userPrompt.at(i - 1)),
+        parseInt(userPrompt.at(i + 1))
+      );
+      userPrompt[i + 1] = calculation;
+    } else if (userPrompt.at(i) === "/") {
+      calculation = divideNumbers(
+        parseInt(userPrompt.at(i - 1)),
+        parseInt(userPrompt.at(i + 1))
+      );
+      userPrompt[i + 1] = calculation;
+    }
   }
+  return calculation;
 }
 
+// FRONT-END
+
+let inputCheck = document.querySelectorAll(".input");
+let display = document.querySelector(".display");
+
+let inputNumber = "";
+let inputParsed = "";
+
+inputCheck.forEach((input) => {
+  input.addEventListener("click", () => {
+    if (
+      input.id === "+" ||
+      input.id === "-" ||
+      input.id === "/" ||
+      input.id === "*"
+    ) {
+      // Append the current number to the expression
+      inputParsed = inputParsed + parseInt(inputNumber) + " ";
+
+      // If we already have a full expression, evaluate it before appending the new operator
+      let tokens = inputParsed.trim().split(" ");
+      if (tokens.length >= 3) {
+        inputParsed = calculateExpression(inputParsed) + " ";
+      }
+
+      // Add the operator
+      inputParsed = inputParsed + input.id + " ";
+      inputNumber = "";
+    } else if (input.id === "=") {
+      inputParsed = inputParsed + parseInt(inputNumber);
+      let result = calculateExpression(inputParsed);
+      display.innerText = result;
+      inputParsed = "";
+      inputNumber = "";
+      return;
+    } else if (input.id === "clear") {
+      inputParsed = "";
+      inputNumber = "";
+    } else if (input.id === "delete") {
+      inputNumber = inputNumber.slice(0, -1);
+    } else {
+      inputNumber = inputNumber + input.id;
+    }
+
+    // Update the display
+    display.innerText = inputParsed + inputNumber;
+  });
+});
