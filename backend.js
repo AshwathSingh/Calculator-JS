e222e9d283a4ee49987be7feaefd5f411c551580
Ashwath <ashwath.singh2005@gmail.com>
2025-06-07 11:55:59 +0530

    Backend of Calculator App

--- backend.js
+++ backend.js
@@ -1,28 +1,57 @@
-// creating a 4x4 grid
+// create the ui for the calculator
+// enable onclick events such that when one button is clicked, it can operate on it
+// create ids for each one so that it knows what button has been entered <- maybe add
+// this to a running string and if there is one set of operators within it calculate those
+// and then continue the operation
 
-let numberofSquares = parseInt(prompt("What Size Grid Would You Like"));
+// OPERATORS
 
-if (numberofSquares > 48) {
-  numberofSquares = parseInt(prompt("Please Enter a Number Less than 48"));
+function addNumbers(num1, num2) {
+  return num1 + num2;
 }
 
-if (numberofSquares % 4 != 0) {
-  numberofSquares += Math.floor(numberofSquares / 4);
-}
-const builder = document.querySelector(".container");
-
-for (let i = 0; i < numberofSquares; i++) {
-  const gridBox = document.createElement("div");
-  gridBox.classList.add("item"); // Remove the dot here
-  gridBox.id = `item ${i}`;
-
-  builder.append(gridBox);
+function subtractNumbers(num1, num2) {
+  return num1 - num2;
 }
 
-const items = document.querySelectorAll(".item");
+function divideNumbers(num1, num2) {
+  return num1 / num2;
+}
 
-items.forEach((item) => {
-  item.addEventListener("click", () => {
-    item.style.opacity = parseFloat(getComputedStyle(item).opacity) + 0.1;
-  });
-});
+function multiplyNumbers(num1, num2) {
+  return num1 * num2;
+}
+
+// BACKEND
+
+// input from the user as a string
+let userPrompt = prompt(
+  "Please enter the mathematical expression you would like to evaluate"
+);
+
+userPrompt = userPrompt.split(" ");
+
+// process the input to find which function is required and also if it contains multiple operators
+let calculation = 0;
+for (let i = 0; i < userPrompt.length; i++) {
+  // if doesn't below condition must be a number
+
+  if (userPrompt.at(i) === "+") {
+    console.log("add");
+    calculation = addNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
+    userPrompt[i+1] = calculation;
+  } else if (userPrompt.at(i) === "-") {
+    console.log("subtract");
+    calculation = subtractNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
+  } else if (userPrompt.at(i) === "*") {
+    console.log("multiple");
+    calculation = multiplyNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
+    userPrompt.at(i+1) = calculation;
+    userPrompt[i+1] = calculation;
+  } else if (userPrompt.at(i) === "/") {
+    console.log("divide");
+    calculation = divideNumbers(parseInt(userPrompt.at(i - 1)), parseInt(userPrompt.at(i + 1)));
+    userPrompt[i+1] = calculation;
+  }
+}
+
